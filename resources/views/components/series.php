335418330ebf95220ff1f8a9f68223b41cf35ad2
Riccardo <riccardolipparini0@gmail.com>
2022-01-24 17:09:22 +0100

    edit

--- resources/views/components/series.php
+++ resources/views/components/series.php
@@ -1,8 +1,10 @@
 <main>
     <div class="centerseries">
     <div class="series">
+        <a href="{{ route('SecondPage') }}">
             <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Batman_Comic_Book_-_NARA_-_595420.gif" alt="">
             <h4>BATMAN</h4>
+        </a>
         </div>
         <div class="series">
             <img src="https://upload.wikimedia.org/wikipedia/commons/b/b9/Batman_Comic_Book_-_NARA_-_595420.gif" alt="">
